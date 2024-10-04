128609937d4b1ea5f2aeb31d7954145ce5932072
= <=>
2024-10-04 20:29:19 +0100

    add: mobile view update

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -38,27 +38,27 @@
         <!-- Contact Info with Icons -->
         <div class="customer-contact">
           <div class="contact-item">
-            <img src="https://img.icons8.com/ios/50/000000/email.png" alt="Email Icon" class="contact-icon" />
+            <img src="https://img.icons8.com/ios/50/015989/email.png" alt="Email Icon" class="contact-icon" />
             <p>{{ customer.email }}</p>
           </div>
           <div class="contact-item">
-            <img src="https://img.icons8.com/ios/50/000000/marker.png" alt="Location Icon" class="contact-icon" />
+            <img src="https://img.icons8.com/ios/50/015989/marker.png" alt="Location Icon" class="contact-icon" />
             <p>{{ customer.address.street }}, {{ customer.address.city }}</p>
           </div>
           <div class="contact-item">
-            <img src="https://img.icons8.com/ios/50/000000/phone.png" alt="Phone Icon" class="contact-icon" />
+            <img src="https://img.icons8.com/ios/50/015989/phone.png" alt="Phone Icon" class="contact-icon" />
             <p>{{ customer.phone }}</p>
           </div>
           <div class="contact-item">
-            <img src="https://img.icons8.com/ios/50/000000/domain.png" alt="Website Icon" class="contact-icon" />
+            <img src="https://img.icons8.com/ios/50/015989/domain.png" alt="Website Icon" class="contact-icon" />
             <p>{{ customer.website }}</p>
           </div>
           <div class="contact-item">
-            <img src="https://img.icons8.com/ios/50/000000/briefcase.png" alt="Company Icon" class="contact-icon" />
+            <img src="https://img.icons8.com/ios/50/015989/briefcase.png" alt="Company Icon" class="contact-icon" />
             <p>{{ customer.company.name }}</p>
           </div>
           <div class="contact-item">
-            <img src="https://img.icons8.com/ios/50/000000/factory.png" alt="Factory Icon" class="contact-icon" />
+            <img src="https://img.icons8.com/ios/50/015989/factory.png" alt="Factory Icon" class="contact-icon" />
             <p>{{ customer.company.bs }}</p>
           </div>
         </div>
@@ -78,7 +78,7 @@
 .controls {
   display: flex;
   gap: 10px;
-  margin-bottom: 20px;
+  margin-bottom: 80px;
   position: relative;
   flex-wrap: wrap;
 }
@@ -87,6 +87,7 @@
   .controls {
     flex-direction: column;
     align-items: flex-start;
+    margin-bottom: 80px;
   }
 
   .sort-dropdown {
@@ -117,7 +118,7 @@
 }
 
 .search-input {
-  font-size: 16px;
+  font-size: 20px;
   width: inherit;
   height: inherit;
   outline: none;
@@ -179,14 +180,20 @@
 
 .customer-card {
   font-family: Rubik;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
   width: 100%;
-  max-width: 250px;
+  max-width: 380px;
+  min-height: 410px;
   background-color: #f9f9f9;
   border: 1px solid #ddd;
   border-radius: 8px;
   overflow: hidden;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   transition: transform 0.3s ease-in-out;
+  margin-bottom: 25px;
+
 }
 
 .customer-card:hover {
@@ -268,6 +275,8 @@
     display: flex;
     justify-content: space-between;
     flex-direction: row-reverse !important;
+    padding-top: 20px;
+    padding-right: 20px;
   }
 
   .profile-img {
